26834712316a3118732dd77f93db4e2ec59d624c
Admin <admin@Admins-MacBook-Pro.local>
2018-07-20 09:35:29 +0530

    added search engine

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 let React =  require('react');
 let ReactDOM =  require('react-dom');
-
+let products = require('./products');
 
 class Search extends React.Component {
     constructor(props)
@@ -19,7 +19,7 @@ class Search extends React.Component {
         return(
             <div className="search">
                 <SearchBar onQuery={this.handleQuery.bind(this)}/>
-                <Result query={this.state}/>
+                <Results products = {this.props.products}query={this.state.query}/>
             </div>
         )    }
 }
@@ -37,22 +37,48 @@ class SearchBar extends React.Component{
 }
 
 
-class Result extends  React.Component{
+class Results extends  React.Component{
+
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            foundProducts : props.products
+        }
+    }
 
     componentWillReceiveProps(nextProps)
     {
         console.log("results"+ nextProps.query);
+        console.log(nextProps.products);
+        let foundProducts = nextProps.products.filter( product => {
+            return product.name.toLowerCase().match(nextProps.query.toLowerCase()) || product.description.toLowerCase().match(nextProps.query.toLowerCase())
+        })
+        console.log(foundProducts);
+        this.setState({foundProducts: foundProducts});
     }
 
     render() {
-        debugger
-        return(<div className="results">
-            <div className="in-stock">
-                <h2><a href="#">ToothPaster</a></h2>
-                <p>$2.99</p>
-                <p>This is toothpaster in stock</p>
-            </div>
+        return(<div>
+            {this.state.foundProducts.map(
+                function (product, i) {
+                    return <Result product={product} key={i}/>
+                }
+            )}
         </div>);
     }
 }
-ReactDOM.render(<Search/>, document.getElementById('app'))
+
+class Result extends React.Component {
+    render()
+    {
+        return (<div className="results">
+            <div className="in-stock">
+                <h2><a href="#">{this.props.product.name}</a></h2>
+                <p>{this.props.product.price}</p>
+                <p>{this.props.product.description}</p>
+            </div>
+        </div>)
+    }
+}
+ReactDOM.render(<Search products={products}/>, document.getElementById('app'))
